4a85ecd8929b7f708371ef9ab0e21b605c1ea99a
Yuanyuan <bing.vv@gmail.com>
2017-08-24 21:20:35 +1200

    added class user & project

---
+++ NotMyFault/Models/ProjRelated/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotMyFault.Models.ProjRelated
+{
+    public class Project
+    {
+        public string ProjName { get; set; }
+        public string BriefDescription { get; set; }
+        public string FullDescription { get; set; }
+        public DateTime StartingDate { get; set; }
+        public int Capacity { get; set; }
+    }
+}
---
+++ NotMyFault/Models/UserRelated/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotMyFault.Models.UserRelated
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class Developer : User
+    {
+        public int Age { get; set; }
+        public string EmailAddr { get; set; }
+    }
+
+    public class Buyer : User
+    {
+        public string CompanyName { get; set; }
+        public string CompanyAddr { get; set; }
+    }
+}
